259dfe5eb1cc1b2ebd20e97c7606bd5b7e5f4136
egorchecpc <egorchecpc@gmail.com>
2023-11-26 18:20:37 +0300

    First commit

---
+++ constants/index.js
@@ -0,0 +1,77 @@
+// we will be using this dummy data,
+// don't worry, you can find this data in the git repo 
+// you can find the git repo link in the description ;)
+
+export const categories = [
+  {
+    id: 1,
+    title: "Chest",
+  },
+  {
+    id: 2,
+    title: "Legs",
+  },
+  {
+    id: 3,
+    title: "Abs",
+  },
+  {
+    id: 4,
+    title: "Back",
+  },
+  {
+    id: 5,
+    title: "Biceps",
+  },
+]
+
+export const coffeeItems = [
+  {
+    id: 1,
+    name: 'Chest',
+    count: '2',
+    important: '7/10',
+    stars: '4.6',
+    image: require('../assets/images/muscles1.png'),
+    desc: 'The taste of coffee can vary depending on factors such as the type of beans, roast level, brewing method, and the addition of any flavors or sweeteners.'
+  },
+  
+  {
+    id: 2,
+    name: 'Legs',
+    count: '3',
+    important: '8/10',
+    stars: '4.3',
+    image: require('../assets/images/muscles2.png'),
+    desc: 'The taste of coffee can vary depending on factors such as the type of beans, roast level, brewing method, and the addition of any flavors or sweeteners.'
+  },
+  
+  {
+    id: 3,
+    name: 'Abs',
+    count: '3',
+    important: '3/10',
+    stars: '4.0',
+    image: require('../assets/images/muscles3.png'),
+    desc: 'The taste of coffee can vary depending on factors such as the type of beans, roast level, brewing method, and the addition of any flavors or sweeteners.'  },
+  
+  {
+    id: 4,
+    name: 'Back',
+    count: '2',
+    important: '10/10',
+    stars: '3.5',
+    image: require('../assets/images/muscles4.png'),
+    desc: 'The taste of coffee can vary depending on factors such as the type of beans, roast level, brewing method, and the addition of any flavors or sweeteners.'  },
+  
+  {
+    id: 5,
+    name: 'Biceps',
+    count: '2',
+    important: '5/10',
+    stars: '4.7',
+    image: require('../assets/images/muscles5.png'),
+    desc: 'The taste of coffee can vary depending on factors such as the type of beans, roast level, brewing method, and the addition of any flavors or sweeteners.'  },
+  
+
+]
